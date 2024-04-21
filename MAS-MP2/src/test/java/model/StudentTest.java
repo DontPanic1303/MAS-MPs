89680e48558b498fd4a37469f0299441c7ee4120
DontPanic1303 <118015465+DontPanic1303@users.noreply.github.com>
2024-04-21 17:06:36 +0200

    test implementation of student, subject

--- MAS-MP2/src/test/java/model/StudentTest.java
+++ MAS-MP2/src/test/java/model/StudentTest.java
@@ -1,7 +1,299 @@
 package model;
 
+import exceptions.AttributeConstraintViolationException;
+import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+
+import java.time.LocalDate;
+
 import static org.junit.jupiter.api.Assertions.*;
 
-class StudentTest {
+public class StudentTest {
+
+    @Test
+    public void createStudentWithAllArgumentsSuccess(){
+        LocalDate birthdate = LocalDate.of(2000,1,1);
+        Student student = new Student(
+                "Jan",
+                "Kowalski",
+                birthdate,
+                "jan.kowalski@wp.pl"
+        );
+
+        Assertions.assertEquals("Jan",student.getName());
+        Assertions.assertEquals("Kowalski",student.getSurName());
+        Assertions.assertEquals("jan.kowalski@wp.pl",student.getEmail());
+        Assertions.assertEquals(birthdate,student.getBirthDate());
+    }
+
+    @Test
+    public void createStudentWithNameNull(){
+        assertThrows(AttributeConstraintViolationException.class,
+                () -> {
+                    LocalDate birthdate = LocalDate.of(2000,1,1);
+                    Student student = new Student(
+                            null,
+                            "Kowalski",
+                            birthdate,
+                            "jan.kowalski@wp.pl"
+                    );
+                }
+        );
+    }
+
+    @Test
+    public void createStudentWithSurnameNull(){
+        assertThrows(AttributeConstraintViolationException.class,
+                () -> {
+                    LocalDate birthdate = LocalDate.of(2000,1,1);
+                    Student student = new Student(
+                            "Jan",
+                            null,
+                            birthdate,
+                            "jan.kowalski@wp.pl"
+                    );
+                }
+        );
+    }
+
+    @Test
+    public void createStudentWithBirthdateNull(){
+        assertThrows(AttributeConstraintViolationException.class,
+                () -> {
+                    LocalDate birthdate = LocalDate.of(2000,1,1);
+                    Student student = new Student(
+                            "Jan",
+                            "Kowalski",
+                            null,
+                            "jan.kowalski@wp.pl"
+                    );
+                }
+        );
+    }
+
+    @Test
+    public void createStudentWithEmailNull(){
+        assertThrows(AttributeConstraintViolationException.class,
+                () -> {
+                    LocalDate birthdate = LocalDate.of(2000,1,1);
+                    Student student = new Student(
+                            "Jan",
+                            "Kowalski",
+                            birthdate,
+                            null
+                    );
+                }
+        );
+    }
+
+    @Test
+    public void createStudentWithNameEmpty(){
+        assertThrows(AttributeConstraintViolationException.class,
+                () -> {
+                    LocalDate birthdate = LocalDate.of(2000,1,1);
+                    Student student = new Student(
+                            "",
+                            "Kowalski",
+                            birthdate,
+                            "jan.kowalski@wp.pl"
+                    );
+                }
+        );
+    }
+
+    @Test
+    public void createStudentWithSurnameEmpty(){
+        assertThrows(AttributeConstraintViolationException.class,
+                () -> {
+                    LocalDate birthdate = LocalDate.of(2000,1,1);
+                    Student student = new Student(
+                            "Jan",
+                            "",
+                            birthdate,
+                            "jan.kowalski@wp.pl"
+                    );
+                }
+        );
+    }
+
+    @Test
+    public void createStudentWithEmailEmpty(){
+        assertThrows(AttributeConstraintViolationException.class,
+                () -> {
+                    LocalDate birthdate = LocalDate.of(2000,1,1);
+                    Student student = new Student(
+                            "Jan",
+                            "Kowalski",
+                            birthdate,
+                            ""
+                    );
+                }
+        );
+    }
+
+    @Test
+    public void setNameSuccess(){
+        LocalDate birthdate = LocalDate.of(2000,1,1);
+        Student student = new Student(
+                "Jan",
+                "Kowalski",
+                birthdate,
+                "jan.kowalski@wp.pl"
+        );
+        student.setName("Paweł");
+        Assertions.assertEquals("Paweł",student.getName());
+    }
+
+    @Test
+    public void setNameNull(){
+        LocalDate birthdate = LocalDate.of(2000,1,1);
+        Student student = new Student(
+                "Jan",
+                "Kowalski",
+                birthdate,
+                "jan.kowalski@wp.pl"
+        );
+        assertThrows(AttributeConstraintViolationException.class,
+                () -> {
+                    student.setName(null);
+                }
+        );
+    }
+
+    @Test
+    public void setNameEmpty(){
+        LocalDate birthdate = LocalDate.of(2000,1,1);
+        Student student = new Student(
+                "Jan",
+                "Kowalski",
+                birthdate,
+                "jan.kowalski@wp.pl"
+        );
+        assertThrows(AttributeConstraintViolationException.class,
+                () -> {
+                    student.setName("");
+                }
+        );
+    }
+
+    @Test
+    public void setSurnameSuccess(){
+        LocalDate birthdate = LocalDate.of(2000,1,1);
+        Student student = new Student(
+                "Jan",
+                "Kowalski",
+                birthdate,
+                "jan.kowalski@wp.pl"
+        );
+        student.setSurName("Kowal");
+        Assertions.assertEquals("Kowal",student.getSurName());
+    }
+
+    @Test
+    public void setSurnameNull(){
+        LocalDate birthdate = LocalDate.of(2000,1,1);
+        Student student = new Student(
+                "Jan",
+                "Kowalski",
+                birthdate,
+                "jan.kowalski@wp.pl"
+        );
+        assertThrows(AttributeConstraintViolationException.class,
+                () -> {
+                    student.setSurName(null);
+                }
+        );
+    }
+
+    @Test
+    public void setSurnameEmpty(){
+        LocalDate birthdate = LocalDate.of(2000,1,1);
+        Student student = new Student(
+                "Jan",
+                "Kowalski",
+                birthdate,
+                "jan.kowalski@wp.pl"
+        );
+        assertThrows(AttributeConstraintViolationException.class,
+                () -> {
+                    student.setSurName("");
+                }
+        );
+    }
+
+    @Test
+    public void setBirthdateSuccess(){
+        LocalDate birthdate = LocalDate.of(2000,1,1);
+        Student student = new Student(
+                "Jan",
+                "Kowalski",
+                birthdate,
+                "jan.kowalski@wp.pl"
+        );
+        birthdate = LocalDate.of(2000,1,2);
+        student.setBirthDate(birthdate);
+        Assertions.assertEquals(birthdate,student.getBirthDate());
+    }
+
+    @Test
+    public void setBirthdateNull(){
+        LocalDate birthdate = LocalDate.of(2000,1,1);
+        Student student = new Student(
+                "Jan",
+                "Kowalski",
+                birthdate,
+                "jan.kowalski@wp.pl"
+        );
+        assertThrows(AttributeConstraintViolationException.class,
+                () -> {
+                    student.setBirthDate(null);
+                }
+        );
+    }
+
+    @Test
+    public void setEmailSuccess(){
+        LocalDate birthdate = LocalDate.of(2000,1,1);
+        Student student = new Student(
+                "Jan",
+                "Kowalski",
+                birthdate,
+                "jan.kowalski@wp.pl"
+        );
+        student.setEmail("jan.kowalski2@wp.pl");
+        Assertions.assertEquals("jan.kowalski2@wp.pl",student.getEmail());
+    }
+
+    @Test
+    public void setEmailNull(){
+        LocalDate birthdate = LocalDate.of(2000,1,1);
+        Student student = new Student(
+                "Jan",
+                "Kowalski",
+                birthdate,
+                "jan.kowalski@wp.pl"
+        );
+        assertThrows(AttributeConstraintViolationException.class,
+                () -> {
+                    student.setEmail(null);
+                }
+        );
+    }
+
+    @Test
+    public void setEmailEmpty(){
+        LocalDate birthdate = LocalDate.of(2000,1,1);
+        Student student = new Student(
+                "Jan",
+                "Kowalski",
+                birthdate,
+                "jan.kowalski@wp.pl"
+        );
+        assertThrows(AttributeConstraintViolationException.class,
+                () -> {
+                    student.setEmail("");
+                }
+        );
+    }
 
 }
--- MAS-MP2/src/test/java/model/SubjectTest.java
+++ MAS-MP2/src/test/java/model/SubjectTest.java
@@ -1,9 +1,143 @@
 package model;
 
-import org.junit.jupiter.api.Test;
+import exceptions.AttributeConstraintViolationException;
+import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+
+import java.time.LocalDate;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class SubjectTest {
+public class SubjectTest {
+    @Test
+    public void createSubjectWithAllArgumentsSuccess(){
+        Subject subject = new Subject(
+                "english",
+                "B2"
+        );
+
+        Assertions.assertEquals("english",subject.getName());
+        Assertions.assertEquals("B2",subject.getLevel());
+    }
+
+    @Test
+    public void createSubjectWithNameNull(){
+        assertThrows(AttributeConstraintViolationException.class,
+                () -> {
+                    Subject subject = new Subject(
+                            null,
+                            "B2"
+                    );
+                }
+        );
+    }
+
+    @Test
+    public void createSubjectWithLevelNull(){
+        assertThrows(AttributeConstraintViolationException.class,
+                () -> {
+                    Subject subject = new Subject(
+                            "english",
+                            null
+                    );
+                }
+        );
+    }
+
+    @Test
+    public void createSubjectWithNameEmpty(){
+        assertThrows(AttributeConstraintViolationException.class,
+                () -> {
+                    Subject subject = new Subject(
+                            "",
+                            "B2"
+                    );
+                }
+        );
+    }
+
+    @Test
+    public void createSubjectWithLevelEmpty(){
+        assertThrows(AttributeConstraintViolationException.class,
+                () -> {
+                    Subject subject = new Subject(
+                            "english",
+                            ""
+                    );
+                }
+        );
+    }
+
+    @Test
+    public void setNameSuccess(){
+        Subject subject = new Subject(
+                "english",
+                "B2"
+        );
+        subject.setName("polish");
+        Assertions.assertEquals("polish",subject.getName());
+    }
+
+    @Test
+    public void setNameNull(){
+        Subject subject = new Subject(
+                "english",
+                "B2"
+        );
+        assertThrows(AttributeConstraintViolationException.class,
+                () -> {
+                    subject.setName(null);
+                }
+        );
+    }
+
+    @Test
+    public void setNameEmpty(){
+        Subject subject = new Subject(
+                "english",
+                "B2"
+        );
+        assertThrows(AttributeConstraintViolationException.class,
+                () -> {
+                    subject.setName("");
+                }
+        );
+    }
+
+    @Test
+    public void setLevelSuccess(){
+        Subject subject = new Subject(
+                "english",
+                "B2"
+        );
+        subject.setLevel("B1");
+        Assertions.assertEquals("B1",subject.getLevel());
+    }
+
+    @Test
+    public void setLevelNull(){
+        Subject subject = new Subject(
+                "english",
+                "B2"
+        );
+        assertThrows(AttributeConstraintViolationException.class,
+                () -> {
+                    subject.setLevel(null);
+                }
+        );
+    }
+
+    @Test
+    public void setLevelEmpty(){
+        Subject subject = new Subject(
+                "english",
+                "B2"
+        );
+        assertThrows(AttributeConstraintViolationException.class,
+                () -> {
+                    subject.setLevel("");
+                }
+        );
+    }
 
 }
